55516d3776713667f3e20370e73eaca7693cc72c
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2024-01-30 08:33:17 +0530

    update created

--- app/src/main/java/com/iert/ytdisfree/MyAdapter.java
+++ app/src/main/java/com/iert/ytdisfree/MyAdapter.java
@@ -1,25 +1,39 @@
 package com.iert.ytdisfree;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     Context context;
@@ -46,6 +60,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
         this.holder = holder;
         this.position = position;
+
+        TextView title;
 
 
         VideoList url = urlArrayList.get(position);
@@ -108,6 +124,91 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             }
         });
 
+        Dialog dialog = new Dialog(context);
+
+        dialog.setContentView(R.layout.dialog_add_box);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(context.getDrawable(R.drawable.dia_bg));
+
+holder.title.setOnLongClickListener(new View.OnLongClickListener() {
+    @Override
+    public boolean onLongClick(View v) {
+        dialog.show();
+        dialog.setCancelable(false);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
+
+
+        holder.addDiaButton = dialog.findViewById(R.id.addData);
+        holder.cancelBtn = dialog.findViewById(R.id.cancelDialog);
+        holder.urlEditText = dialog.findViewById(R.id.editText);
+        holder.titleEditText = dialog.findViewById(R.id.editTitle);
+
+        holder.addDiaButton.setText("Update");
+        //click to add url
+        holder.addDiaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!holder.urlEditText.getText().toString().equals("") && !holder.titleEditText.getText().toString().equals("")) {
+                    ProgressDialog pd;
+                    pd = new ProgressDialog(context);
+                    pd.setCancelable(false);
+                   pd.setMessage("Updating data...");
+                    pd.show();
+
+
+                    SimpleDateFormat sdf =new  SimpleDateFormat("dd - MM - yyyy ' ' HH : mm : ss z");
+                    String current = sdf.format(new Date());
+                    Map<String, Object> url = new HashMap<>();
+                    url.put("url", holder.urlEditText.getText().toString());
+                    url.put("title", holder.titleEditText.getText().toString());
+                    url.put("date", current);
+                    //adding data
+                    db.collection("url") .document(urlArrayList.get(position).getDocumentId())
+                           .set(url)
+
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(context, "success", Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                    holder.urlEditText.setText(null);
+                                    holder.titleEditText.setText(null);
+                                    pd.dismiss();
+
+
+
+
+                                }
+                                })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    pd.dismiss();
+                                    Toast.makeText(context, "error ocurred", Toast.LENGTH_SHORT).show();
+                                    holder.urlEditText.setText(null);
+                                    holder.titleEditText.setText(null);
+                                }
+                            });
+
+                } else {
+                    Toast.makeText(context, "Enter title and url", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        holder.cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+
+            }
+        });
+
+        return false;
+    }
+});
 
     }
 
@@ -119,7 +220,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView title;
         TextView url;
-        Button delete;
+        Button delete, addDiaButton, cancelBtn;
+        EditText urlEditText,titleEditText;
+
+
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
